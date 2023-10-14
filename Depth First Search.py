bfdc6d4ecd034648d880a13fa5b2eacbc1e644d4
Vatsal Singh <96788937+Vatsal-Singh23@users.noreply.github.com>
2023-10-14 23:44:50 +0530

    Create Depth First Search.py

---
+++ Depth First Search.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def dfs(self, node, visited):
+        visited[node] = True
+        print(node, end=' ')
+
+        for neighbor in self.graph[node]:
+            if not visited[neighbor]:
+                self.dfs(neighbor, visited)
+
+    def dfs_traversal(self, start_node):
+        visited = [False] * (max(self.graph) + 1)
+        self.dfs(start_node, visited)
+
+# Example usage:
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 0)
+g.add_edge(2, 3)
+g.add_edge(3, 3)
+
+print("Depth First Traversal (starting from vertex 2):")
+g.dfs_traversal(2)
